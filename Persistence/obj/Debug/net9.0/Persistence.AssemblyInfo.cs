85976305021858b7e50ff967cdeeed4067462162
Bina <dyakovabina@gmail.com>
2025-09-16 22:05:43 +0200

    Update gitignore

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a852415bebc6bf69e7096c30fd98e5cbf234ac63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba931a653c6b6d86317c605cd93cb10a188a5599")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
