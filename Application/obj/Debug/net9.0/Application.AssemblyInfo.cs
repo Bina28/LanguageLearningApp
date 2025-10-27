1bdb964f03104a7b07bd42c2a145298fb21e52bd
Bina <dyakovabina@gmail.com>
2025-10-27 20:14:25 +0100

    Refactor code structure for improved readability and maintainability

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ec6906f29fed7448aad8d22a3705103512a751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee8ccb1b8efff9a64162d3facf10753eb0bec53e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
